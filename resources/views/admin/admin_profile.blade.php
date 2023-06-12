21f7a22012eefd3b438dd191d6486fb4d3019ade
sonucjha <nabnitcjha@gmil.com>
2023-06-12 22:32:35 +0545

    dminlout route

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -3,61 +3,68 @@
 @section('admin')
 <div class="content">
 
-<!-- Start Content-->
-<div class="container-fluid">
+    <!-- Start Content-->
+    <div class="container-fluid">
 
-    <!-- start page title -->
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box">
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">Contacts</a></li>
-                        <li class="breadcrumb-item active">Profile</li>
-                    </ol>
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box">
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Contacts</a></li>
+                            <li class="breadcrumb-item active">Profile</li>
+                        </ol>
+                    </div>
+                    <h4 class="page-title">Profile</h4>
                 </div>
-                <h4 class="page-title">Profile</h4>
             </div>
         </div>
-    </div>     
-    <!-- end page title -->
+        <!-- end page title -->
 
-    <div class="row">
-        <div class="col-lg-4 col-xl-4">
-            <div class="card text-center">
-                <div class="card-body">
-                    <img src="{{asset('backend/assets/images/users/user-1.jpg')}}" class="rounded-circle avatar-lg img-thumbnail"
-                    alt="profile-image">
+        <div class="row">
+            <div class="col-lg-4 col-xl-4">
+                <div class="card text-center">
+                    <div class="card-body">
+                        @if ($adminData->photo == null)
+                        <img src="{{ asset('upload/admin_image/no_image.jpg') }}" class="rounded-circle avatar-lg img-thumbnail" alt="profile-image">
+                        @else
+                        <img src="{{ asset('backend/assets/images/users/user-1.jpg') }}" class="rounded-circle avatar-lg img-thumbnail" alt="profile-image">
+                        @endif
 
-                    <h4 class="mb-0">{{$adminData->name}}</h4>
-                    <p class="text-muted">@ {{$adminData->username}}</p>
 
-                    <button type="button" class="btn btn-success btn-xs waves-effect mb-2 waves-light">Follow</button>
-                    <button type="button" class="btn btn-danger btn-xs waves-effect mb-2 waves-light">Message</button>
 
-                    <div class="text-start mt-3">
-                       
-                        <p class="text-muted mb-2 font-13"><strong>Full Name :</strong> <span class="ms-2">{{$adminData->name}}</span></p>
-                    
-                        <p class="text-muted mb-2 font-13"><strong>Mobile :</strong><span class="ms-2">{{$adminData->phone}}</span></p>
-                    
-                        <p class="text-muted mb-2 font-13"><strong>Email :</strong> <span class="ms-2">{{$adminData->email}}</span></p>
 
-                    </div>                                    
+                        <h4 class="mb-0">{{ $adminData->name }}</h4>
+                        <p class="text-muted">@ {{ $adminData->username }}</p>
 
-                     
-                </div>                                 
-            </div> <!-- end card -->
+                        <button type="button" class="btn btn-success btn-xs waves-effect mb-2 waves-light">Follow</button>
+                        <button type="button" class="btn btn-danger btn-xs waves-effect mb-2 waves-light">Message</button>
 
-        </div> <!-- end col-->
+                        <div class="text-start mt-3">
 
-        <div class="col-lg-8 col-xl-8">
-            <div class="card">
-                <div class="card-body">
+                            <p class="text-muted mb-2 font-13"><strong>Full Name :</strong> <span class="ms-2">{{ $adminData->name }}</span></p>
+
+                            <p class="text-muted mb-2 font-13"><strong>Mobile :</strong><span class="ms-2">{{ $adminData->phone }}</span></p>
+
+                            <p class="text-muted mb-2 font-13"><strong>Email :</strong> <span class="ms-2">{{ $adminData->email }}</span></p>
+
+                        </div>
+
+
+                    </div>
+                </div> <!-- end card -->
+
+            </div> <!-- end col-->
+
+            <div class="col-lg-8 col-xl-8">
+                <div class="card">
+                    <div class="card-body">
                         <div class="tab-pane" id="settings">
                             <form>
-                                <h5 class="mb-4 text-uppercase"><i class="mdi mdi-account-circle me-1"></i>Admin Personal Info</h5>
+                                <h5 class="mb-4 text-uppercase"><i class="mdi mdi-account-circle me-1"></i>Admin
+                                    Personal Info</h5>
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="mb-3">
@@ -91,5 +98,4 @@
 </div> <!-- container -->
 
 </div> <!-- content -->
-
 @endSection
