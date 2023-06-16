48c89396f75981849022963ff160dbc7c3611830
sonucjha <nabnitcjha@gmil.com>
2023-06-16 12:23:54 +0545

    dminlout route

--- resources/views/admin/admin_password_change.blade.php
+++ resources/views/admin/admin_password_change.blade.php
@@ -1,11 +1,100 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hello password change</h1>
-</body>
-</html>
+@extends('admin.admin_layout')
+
+@section('admin')
+<div class="content">
+
+    <!-- Start Content-->
+    <div class="container-fluid">
+
+        <!-- start page title -->
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box">
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Contacts</a></li>
+                            <li class="breadcrumb-item active">Change Password</li>
+                        </ol>
+                    </div>
+                    <h4 class="page-title">Change Password</h4>
+                </div>
+            </div>
+        </div>
+        <!-- end page title -->
+
+        <div class="row">
+
+
+            <div class="col-lg-8 col-xl-8">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="tab-pane" id="settings">
+                            <form method="POST" action="{{ url('/admin/password/change') }}" class="needs-validation" novalidate enctype="multipart/form-data">
+                                @csrf
+                                <h5 class="mb-4 text-uppercase"><i class="mdi mdi-account-circle me-1"></i>Admin
+                                    Password Change</h5>
+                                <div class="row">
+
+                                    <div class="col-md-6">
+                                        <div class="mb-3">
+                                            <label for="password" class="form-label">Password</label>
+                                            <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="mb-3">
+                                            <label for="confirm_password" class="form-label"> Confirm Password</label>
+                                            <input type="text" class="form-control" id="confirm_password" name="confirm_password" placeholder="Enter  confirm_password">
+                                            <input type="text" class="form-control invisible" id="user_id" name="user_id" value="{{$adminData->id}}">
+                                        </div>
+                                    </div> <!-- end col -->
+
+
+
+
+                                </div>
+                        </div> <!-- end row -->
+
+                        <div class="text-end">
+                            <button type="submit" class="btn btn-success waves-effect waves-light mt-2"><i class="mdi mdi-content-save"></i> Save</button>
+                        </div>
+                        </form>
+                    </div>
+                    <!-- end settings content-->
+
+                </div> <!-- end tab-content -->
+            </div>
+        </div> <!-- end card-->
+
+    </div> <!-- end col -->
+</div>
+<!-- end row-->
+
+</div> <!-- container -->
+
+</div> <!-- content -->
+
+<script type=text/javascript>
+    var fileTag = document.getElementById("photo"),
+        preview = document.getElementById("showImage");
+
+    fileTag.addEventListener("change", function() {
+        changeImage(this);
+    });
+
+    function changeImage(input) {
+        var reader;
+
+        if (input.files && input.files[0]) {
+            reader = new FileReader();
+
+            reader.onload = function(e) {
+                preview.setAttribute('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
+@endSection
