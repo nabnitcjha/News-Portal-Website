a38c9d6421cf2d884bbed8f652c7100c4d356bec
sonucjha <nabnitcjha@gmil.com>
2023-06-16 13:03:17 +0545

    dminlout route

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -56,20 +56,30 @@ class AdminController extends Controller
 
     public function AdminPasswordChange(Request $request)
     {
-        $this->validate($request, [
-            'password' => 'required|confirmed',
-        ]);
-
-        $adminData = User::find($request->user_id);
-        $adminData->password = Hash::make($request->password);
-        $adminData->save();
-
-         $notification = array(
-            'message'=>'Admin Password Change Successfully',
-            'alert-type'=>'warning'
-        );
-
-        return back()->with($notification);
+        try {
+            $this->validate($request, [
+                'password' => 'required|confirmed',
+            ]);
+    
+            $adminData = User::find($request->user_id);
+            $adminData->password = Hash::make($request->password);
+            $adminData->save();
+    
+             $notification = array(
+                'message'=>'Admin Password Change Successfully',
+                'alert-type'=>'warning'
+            );
+    
+            return back()->with($notification);
+        } catch (\Throwable $th) {
+            $notification = array(
+                'message'=>'Password not match',
+                'alert-type'=>'warning'
+            );
+    
+            return back()->with($notification);
+        }
+       
     }
 
     public function AdminProfileUpdate(Request $request)
