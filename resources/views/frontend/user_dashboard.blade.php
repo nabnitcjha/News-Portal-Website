05bc0907f9064b7e4c33caf875458d9d253b12bb
sonucjha <nabnitcjha@gmil.com>
2023-06-19 18:02:28 +0545

    improve code

--- resources/views/frontend/user_dashboard.blade.php
+++ resources/views/frontend/user_dashboard.blade.php
@@ -14,7 +14,12 @@
 
 
                             <figure class="authorPage-image">
-                                <img alt=""  src="{{asset('frontend/assets/images/lazy.jpg')}}" class="avatar avatar-96 photo" height="96" width="96" loading="lazy">
+                                @if ($userData->photo == null)
+                                <img id="showImage" src="{{ asset('upload/admin_image/no_image.jpg') }}" class="rounded-circle avatar-lg img-thumbnail" alt="profile-image">
+                                @else
+                                <img id="showImage" src="{{ asset('storage/'.$userData->photo )}}" class="rounded-circle avatar-lg img-thumbnail" alt="profile-image">
+                                @endif
+                                <!-- <img alt="" src="{{asset('frontend/assets/images/lazy.jpg')}}" class="avatar avatar-96 photo" height="96" width="96" loading="lazy"> -->
                             </figure>
                             <h1 class="authorPage-name">
                                 <a href=" "> Kazi Ariyan </a>
