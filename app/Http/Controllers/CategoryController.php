d744a141db614bd89e5244a2f0b7647fe49b2d87
sonucjha <nabnitcjha@gmil.com>
2023-06-21 12:44:14 +0545

    improve code

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,8 @@ class CategoryController extends Controller
     public function CategoryAll()
     {
         $adminData = User::find(auth()->user()->id);
-        return view('admin.category_all', compact('adminData'));
+        $category = Category::latest()->get();
+        return view('admin.category_all', compact(['adminData','category']));
     }
 
     public function CategoryAdd()
