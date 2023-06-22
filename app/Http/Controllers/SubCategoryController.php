0e3dcf8cf6b9e0d929b40379dcb27b3d66acc6c6
sonucjha <nabnitcjha@gmil.com>
2023-06-22 19:35:02 +0545

    improve code

--- app/Http/Controllers/SubCategoryController.php
+++ app/Http/Controllers/SubCategoryController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 class SubCategoryController extends Controller
 {
     public function SubCategoryAddPage(){
-
+        return view('admin.subcategory.sub_category_add');
     }
 
     public function SubCategoryAdd(Request $request){
-
+        return view('admin.subcategory.sub_category_all');
     }
 }
